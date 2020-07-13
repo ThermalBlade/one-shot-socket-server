919a3b37cf2799f97640ee0ba223c390eeb4adf6
ThermalBlade <copr5155@colorado.edu>
2020-07-13 15:40:22 -0700

    Changed port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+const port = process.env.port || 3000
+
 app.get('/', (req, res) => {
     res.send('Welcome to the homepage my guy.')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Got this far bruv.")
 })
