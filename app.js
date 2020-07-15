813f9479a0c634b387dbb9aeb74507ab0eb63b05
ThermalBlade <copr5155@colorado.edu>
2020-07-15 10:37:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const index = require("./routes/index")
 app.use(index)
 
 const server = http.createServer(app)
-const port = process.env.PORT || 4001
+const port = process.env.PORT || 4000
 server.listen(port, () => console.log(`Listening on port ${port}`))
 
 const io = socketIo(server)
