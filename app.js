385de5c2d809c8b73ba0c69945af16727701f108
ThermalBlade <copr5155@colorado.edu>
2020-07-30 16:28:36 -0700

    testing for elastic beanstalk

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ server.listen(port, () => console.log(`Listening on port ${port}`))
 
 const io = socketIo(server)
 
-io.on('connection', (socket) => {
+/*io.on('connection', (socket) => {
     console.log('New client connected')
 
     socket.join('some room')
@@ -35,12 +35,22 @@ io.of('/games').on('connection', (socket) => {
     socket.emit('welcome', 'welcome to games area')
 
     socket.on('joinRoom', (room) => {
-        if(gameRooms.includes('room')){
+        if(chatRooms.includes(room)){
             socket.join(room)
+            io.of('/games').in(room).emit('newUser', 'New user in room!')
             return socket.emit('success', 'You joined the room.')
         }
         else{
             return socket.emit('err', 'Room does not exist.')
         }
     })
+})*/
+
+io.of('/chat').on('connection', (socket) => {
+    socket.on('newMsg', (data) => {
+        console.log('new message from ', data.username, data.msg)
+        io.sockets.emit('new message2', data.username + '\n' +  data.msg)
+    })
+
+    //socket.broadcast.emit('newMessage', data)
 })
