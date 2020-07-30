ba96f1f581807133c994cb76ba064cc1b36185ff
ThermalBlade <copr5155@colorado.edu>
2020-07-29 17:02:39 -0700

    working on socket rooms

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
 const express = require("express")
 const http = require("http")
 const socketIo = require("socket.io")
-const redisAdapter = require('socket.io-redis')
 
 const app = express()
 const index = require("./routes/index")
 app.use(index)
 
 const server = http.createServer(app)
-const port = process.env.npm_config_myVar
+const port = 3005
 server.listen(port, () => console.log(`Listening on port ${port}`))
 
 const io = socketIo(server)
-io.adapter(redisAdapter({host: 'localhost', port:'6379'}))
 
 io.on('connection', (socket) => {
     console.log('New client connected')
 
+    socket.join('some room')
+
     socket.on('new message', function (data){
         console.log(data)
         io.sockets.emit('new message2', data)
+        io.to('some room').emit('hello there')
     })
 
     socket.on('disconnect', () => {
         console.log('Client disconnected')
+    })
+})
+
+const chatRooms = ['rocket league', 'csgo', 'bt1']
+
+io.of('/games').on('connection', (socket) => {
+    console.log('New Game Client')
+    socket.emit('welcome', 'welcome to games area')
+
+    socket.on('joinRoom', (room) => {
+        if(gameRooms.includes('room')){
+            socket.join(room)
+            return socket.emit('success', 'You joined the room.')
+        }
+        else{
+            return socket.emit('err', 'Room does not exist.')
+        }
     })
 })
